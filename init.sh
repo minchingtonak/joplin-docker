5ff4a95638dada10981a61c59564a7fee87f050a
Alec <alec.minch@gmail.com>
2023-03-18 17:58:14 -0700

    Enable production db for joplin, add custom networks

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #/usr/bin/env bash
 
-for dir in 'caddy-data' 'caddy-config'; do
+for dir in 'caddy-data' 'caddy-config' 'postgres'; do
     mkdir -p data/"$dir"
 done
 
 export SUBPATH='/example-subpath' # must start with '/'
+
+export POSTGRES_PASSWORD='foobar' # replace with a strong password
 
 docker compose up -d
